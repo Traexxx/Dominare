cfd5669256c29028fca2a4b6364e1544dadd6589
Paulo Eduardo <pg8739232@gmail.com>
2024-11-21 14:55:23 -0300

    Tela de cadastro de morador

--- beck-end/api/api/src/main/java/com/dominare/api/model/pessoa/DadosCadastrosPessoa.java
+++ beck-end/api/api/src/main/java/com/dominare/api/model/pessoa/DadosCadastrosPessoa.java
@@ -3,12 +3,11 @@ package com.dominare.api.model.pessoa;
 import com.dominare.api.model.apartamento.DadosApartamento;
 
 public record DadosCadastrosPessoa(
-    String nome,
-    String dataDeNascimento,
-    String cpf,
-    String telefone,
-    DadosApartamento apartamento,
-    Relacao relacao
-) {
-    
+        String nome,
+        String dataDeNascimento,
+        String cpf,
+        String telefone,
+        DadosApartamento apartamento,
+        Relacao relacao) {
+
 }
